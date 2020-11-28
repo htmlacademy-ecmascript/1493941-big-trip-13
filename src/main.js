6820eb586b916debb57e3f2dac3b4be902cd4798
Liri-39 <lirina74@gmail.com>
2020-11-28 10:04:05 +0200

    исправления по замечаниям предыдущего PR

--- src/main.js
+++ src/main.js
@@ -1,30 +1,12 @@
-import {
-  createTripInfoElement
-} from "./view/trip-info.js";
-import {
-  createCostInfoElement
-} from "./view/cost.js";
-import {
-  createMenuElement
-} from "./view/menu.js";
-import {
-  createFilterFormElement
-} from "./view/filter.js";
-import {
-  createSorterFormElement
-} from "./view/sorter.js";
-import {
-  createTripListElement
-} from "./view/trip-list.js";
-import {
-  createTripPointElement
-} from "./view/trip-point.js";
-import {
-  createNewPointElement
-} from "./view/new-form.js";
-import {
-  createEditPointElement
-} from "./view/edit-form.js";
+import {createTripInfoElement} from "./view/trip-info.js";
+import {createCostInfoElement} from "./view/cost.js";
+import {createMenuElement} from "./view/menu.js";
+import {createFilterFormElement} from "./view/filter.js";
+import {createSorterFormElement} from "./view/sorter.js";
+import {createTripListElement} from "./view/trip-list.js";
+import {createTripPointElement} from "./view/trip-point.js";
+import {createNewPointElement} from "./view/new-form.js";
+import {createEditPointElement} from "./view/edit-form.js";
 
 const TASK_COUNT = 3;
 const render = (container, layout, place) => {
@@ -32,20 +14,17 @@ const render = (container, layout, place) => {
 };
 
 const tripMain = document.querySelector(`.trip-main`);
-render(tripMain, createTripInfoElement(), `afterbegin`);
-
 const tripInfo = document.querySelector(`.trip-info`);
-render(tripInfo, createCostInfoElement(), `beforeend`);
-
 const tripControlsTitle = document.querySelector(`.trip-controls :last-child`);
+const tripEvents = document.querySelector(`.trip-events`);
+const tripEventsList = document.querySelector(`.trip-events__list`);
+
+render(tripMain, createTripInfoElement(), `afterbegin`);
+render(tripInfo, createCostInfoElement(), `beforeend`);
 render(tripControlsTitle, createMenuElement(), `beforebegin`);
 render(tripControlsTitle, createFilterFormElement(), `afterend`);
-
-const tripEvents = document.querySelector(`.trip-events`);
 render(tripEvents, createSorterFormElement(), `beforeend`);
 render(tripEvents, createTripListElement(), `beforeend`);
-
-const tripEventsList = document.querySelector(`.trip-events__list`);
 render(tripEventsList, createNewPointElement(), `afterbegin`);
 for (let i = 0; i < TASK_COUNT; i++) {
   render(tripEventsList, createTripPointElement(), `beforeend`);
