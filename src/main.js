d383cf93c2f9c1260479c099d4f499c31cccfaae
Liri-39 <lirina74@gmail.com>
2020-12-09 15:21:15 +0200

    реализована замена елементов по клику

--- src/main.js
+++ src/main.js
@@ -8,29 +8,52 @@ import TripPointView from "./view/trip-point.js";
 import {generateEvent} from "./mocks/event.js";
 import PointEditFormView from "./view/edit-form.js";
 import {render, RenderPosition} from "./util.js";
-
-const EVENT_COUNT = 20;
+const EVENT_COUNT = 0;
 const tripMain = document.querySelector(`.trip-main`);
 const tripControlsTitle = document.querySelector(`.trip-controls :last-child`);
-const tripEvents = document.querySelector(`.trip-events`);
+const tripPointsContainer = document.querySelector(`.trip-events`);
 const eventsPoints = new Array(EVENT_COUNT).fill().map(generateEvent);
 const events = eventsPoints.slice().sort((a, b) => {
   return +a.dateTime.start - b.dateTime.start;
 });
-
 if (EVENT_COUNT) {
-  render(tripEvents, new SorterView().getElement(), RenderPosition.BEFOREEND);
+  render(tripPointsContainer, new SorterView().getElement(), RenderPosition.BEFOREEND);
   render(tripMain, new TripInfoView(events).getElement(), RenderPosition.AFTERBEGIN);
   const tripInfo = document.querySelector(`.trip-info`);
   render(tripInfo, new CostInfoView(events).getElement(), RenderPosition.BEFOREEND);
 }
 render(tripControlsTitle, new MenuView().getElement(), RenderPosition.BEFOREBEGIN);
 render(tripControlsTitle, new FilterFormView().getElement(), RenderPosition.AFTEREND);
-render(tripEvents, new TripListView(events, 200).getElement(), RenderPosition.BEFOREEND);
-const tripEventsList = document.querySelector(`.trip-events__list`);
-
+render(tripPointsContainer, new TripListView(events, 200).getElement(), RenderPosition.BEFOREEND);
+const tripPointsList = document.querySelector(`.trip-events__list`);
+const renderTripPoint = (tripListElement, tripPoint) => {
+  const tripPointComponent = new TripPointView(tripPoint);
+  const tripPointEditComponent = new PointEditFormView(tripPoint);
+  const replaceTripPointToEditForm = () => {
+    tripListElement.replaceChild(tripPointEditComponent.getElement(), tripPointComponent.getElement());
+  };
+  const replaceEditFormToTripPoint = () => {
+    tripListElement.replaceChild(tripPointComponent.getElement(), tripPointEditComponent.getElement());
+  };
+  const onEscKeyDown = (evt) => {
+    if (evt.key === `Escape` || evt.key === `Esc`) {
+      evt.preventDefault();
+      replaceEditFormToTripPoint();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+  tripPointComponent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
+    replaceTripPointToEditForm();
+    document.addEventListener(`keydown`, onEscKeyDown);
+  });
+  tripPointEditComponent.getElement().querySelector(`form`).addEventListener(`submit`, (evt) => {
+    evt.preventDefault();
+    replaceEditFormToTripPoint();
+    document.removeEventListener(`keydown`, onEscKeyDown);
+  });
+  render(tripListElement, tripPointComponent.getElement(), RenderPosition.BEFOREEND);
+};
 
 for (let i = 0; i < Math.min(events.length, EVENT_COUNT); i++) {
-  render(tripEventsList, new TripPointView(events[i]).getElement(), RenderPosition.BEFOREEND);
+  renderTripPoint(tripPointsList, events[i]);
 }
-render(tripEventsList, new PointEditFormView(events[0]).getElement(), RenderPosition.AFTERBEGIN);
