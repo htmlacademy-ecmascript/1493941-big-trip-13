66fbde41a5ee6a755328efd8aa972d359be6f3bc
Liri-39 <lirina74@gmail.com>
2021-01-27 20:24:39 +0200

    изменено отображение точек путешествия в шапке в зависимости от их количества

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -14,9 +14,13 @@ const createTripInfoElement = (tripDestination, tripDuration) => {
 export default class TripInfoView extends AbstractView {
   constructor(points) {
     super();
-    if (points.length) {
-      this._tripDestination = `${dayjs(points[0].dates.start).format(`MMM DD`)}&nbsp;—&nbsp;${dayjs(points[points.length - 1].dates.end).format(`DD`)}`;
-      this._tripDuration = `${points[0].destination} &mdash; ... &mdash; ${points[points.length - 1].destination}`;
+    if (points.length > 3) {
+      this._tripDuration = `${dayjs(points[0].dates.start).format(`MMM DD`)}&nbsp;—&nbsp;${dayjs(points[points.length - 1].dates.end).format(`DD`)}`;
+      this._tripDestination = `${points[0].destination} &mdash; ... &mdash; ${points[points.length - 1].destination}`;
+    }
+    if (points.length > 0 && points.length <= 3) {
+      this._tripDuration = `${dayjs(points[0].dates.start).format(`MMM DD`)}&nbsp;—&nbsp;${dayjs(points[points.length - 1].dates.end).format(`DD`)}`;
+      this._tripDestination = points.map((point) => `${point.destination} &mdash; `);
     }
   }
 
